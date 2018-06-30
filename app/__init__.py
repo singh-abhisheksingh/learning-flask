6f9ebd16442ebd8ce21f771d33246ae4afcb0d33
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-06-30 21:38:43 +0530

    Handled 404 error

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+########### INDEX #######################
 
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+######## PAGE NOT FOUND ERROR #############
+	
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
 	app.run()
