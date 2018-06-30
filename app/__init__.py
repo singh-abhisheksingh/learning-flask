a28ea964fba53c5723149ab67a837134d87ea736
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2018-07-01 00:53:48 +0530

    Added content management in the script

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
+from contentManagement import Content
+
 app = Flask(__name__)
 
 ########### INDEX #######################
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("index.html", TOPIC_LIST=TOPIC_LIST)
 
 ######## PAGE NOT FOUND ERROR #############
 	
